1a4fe79e2e98f3de23df8440c55d8f83496b5432
coldcloude <coldcloude@163.com>
2026-04-14 16:45:45 +0800

    初始化项目

---
+++ kissbot-agent-config/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Kissbot Agent Config</h1>
+    </>
+  )
+}
+
+export default App
---
+++ kissbot-memory-manage/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Kissbot Memory Manage</h1>
+    </>
+  )
+}
+
+export default App
