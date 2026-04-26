56b0ebcc5853db6ecdb720490ec599140b17f392
coldcloude <coldcloude@163.com>
2026-04-27 01:18:43 +0800

    重命名

--- kissbot/src/distinct_index.rs
+++ kissbot/src/distinct_index.rs
@@ -18,11 +18,11 @@ where
     }
 }
 
-pub fn build<T>(contents: impl IntoIterator<Item = Vec<T>>, max_depth: usize) -> SingleIndex<T>
+pub fn build<T>(contents: impl IntoIterator<Item = Vec<T>>, max_depth: usize) -> DistinctIndex<T>
 where
     T: Eq + Hash + Clone + 'static,
 {
-    let mut root = SingleIndex::new();
+    let mut root = DistinctIndex::new();
     for content in contents {
         //取所有长度不超过max_depth的子串进行索引
         for start in 0..content.len() {
@@ -34,7 +34,7 @@ where
             let mut current_tree = &mut root;
             //每个token一个子树
             while let Some(token) = valid_tokens.pop_front() {
-                current_tree = current_tree.sub_tree_map.get_or_insert_with(|| HashMap::new()).entry(token).or_insert_with(|| SingleIndex::new());
+                current_tree = current_tree.sub_tree_map.get_or_insert_with(|| HashMap::new()).entry(token).or_insert_with(|| DistinctIndex::new());
             }
         }
     }
