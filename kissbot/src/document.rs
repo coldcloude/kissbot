a4d903ceefba68ce12914265cb22be11b6aaeef3
coldcloude <coldcloude@163.com>
2026-04-27 11:08:01 +0800

    将kissbot中的基础库移动为单独项目

--- kissbot/src/document.rs
+++
@@ -1,9 +0,0 @@
-pub trait Document {
-    fn contents(&self) -> Vec<String>;
-}
-
-impl<T: ToString> Document for T {
-    fn contents(&self) -> Vec<String> {
-        vec![self.to_string()]
-    }
-}
