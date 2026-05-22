8ddc9c5094dd932bf01bc6553dd0bc5144f318a1
coldcloude <coldcloude@163.com>
2026-05-22 12:22:21 +0800

    增加了messenger、user、group、channel定义

--- kissbot-api/src/channel.rs
+++ kissbot-api/src/channel.rs
@@ -1,20 +1,145 @@
 use kai_ws::{LEN_PAYLOAD_TYPE, OFFSET_PAYLOAD_TYPE};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
 
-use crate::{MapKind, StringKind, error::Result};
+use crate::{LocalMap, LocalString, MapKind, StringKind, error::Result};
 
-//========================= Attachment ==========================
+// ========== Messenger -> User -> Group-> Channel ==========
 
+// ========== Channel Info ==========
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct AttachmentMetadata{
-    pub mime_type: String,
-    pub size_bytes: u64,
+pub struct ChannelInfoGeneric<S>
+where
+    S: StringKind,
+{
+    pub channel_id: S::Type,
+    pub messenger_id: S::Type,
+    pub group_id: S::Type,
+    pub user_id: S::Type,
 }
 
-pub struct OutgoingMessageGeneric<S, M>
+pub trait ChannelInfoKind {
+    type Type: Clone + Serialize + DeserializeOwned;
+}
+
+pub type ChannelInfoEntity = ChannelInfoGeneric<LocalString>;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LocalChannelInfo;
+
+impl ChannelInfoKind for LocalChannelInfo {
+    type Type = ChannelInfoEntity;
+}
+
+// ========== Group Info ==========
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GroupInfoGeneric<S, C>
+where
+    S: StringKind,
+    C: ChannelInfoKind,
+{
+    pub group_id: S::Type,
+    pub group_name: S::Type,
+    pub channel: Option<C::Type>,
+}
+
+pub trait GroupInfoKind {
+    type Type: Clone + Serialize + DeserializeOwned;
+}
+
+pub type GroupInfoEntity = GroupInfoGeneric<LocalString, LocalChannelInfo>;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LocalGroupInfo;
+
+impl GroupInfoKind for LocalGroupInfo {
+    type Type = GroupInfoEntity;
+}
+
+// ========== User Info ==========
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UserInfoGeneric<S, M, G>
 where
     S: StringKind,
     M: MapKind,
+    G: GroupInfoKind,
+{
+    pub user_id: S::Type,
+    pub user_name: S::Type,
+    pub group_map: M::Map<String, G::Type>,
+}
+
+pub trait UserInfoKind {
+    type Type: Clone + Serialize + DeserializeOwned;
+}
+
+pub type UserInfoEntity = UserInfoGeneric<LocalString, LocalMap, LocalGroupInfo>;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LocalUserInfo;
+
+impl UserInfoKind for LocalUserInfo {
+    type Type = UserInfoEntity;
+}
+
+// ========== Messenger Info ==========
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MessengerInfoGeneric<S, M, U>
+where
+    S: StringKind,
+    M: MapKind,
+    U: UserInfoKind,
+{
+    pub messenger_id: S::Type,
+    pub messenger_name: S::Type,
+    pub user_map: M::Map<String, U::Type>,
+}
+
+pub trait MessengerInfoKind {
+    type Type: Clone + Serialize + DeserializeOwned;
+}
+
+pub type MessengerInfoEntity = MessengerInfoGeneric<LocalString, LocalMap, LocalUserInfo>;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LocalMessengerInfo;
+
+impl MessengerInfoKind for LocalMessengerInfo {
+    type Type = MessengerInfoEntity;
+}
+
+// ========================= Message & Attachment ==========================
+
+// ========== Attachment Info ==========
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AttachmentInfoGeneric<S>
+where
+    S: StringKind,
+{
+    pub att_id: S::Type,
+    pub mime_type: S::Type,
+    pub size_bytes: u64,
+}
+
+pub trait AttachmentInfoKind {
+    type Type: Clone + Serialize + DeserializeOwned;
+}
+
+pub type AttachmentInfoEntity = AttachmentInfoGeneric<LocalString>;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LocalAttachmentInfo;
+
+impl AttachmentInfoKind for LocalAttachmentInfo {
+    type Type = AttachmentInfoEntity;
+}
+
+// ========== Outgoing Message ==========
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct OutgoingMessageGeneric<S, M, A>
+where
+    S: StringKind,
+    M: MapKind,
+    A: AttachmentInfoKind,
 {
     pub channel_id: S::Type,
     pub user_id: S::Type,
@@ -22,9 +147,13 @@ where
     pub msg_type: S::Type,
     pub content: S::Type,
     pub time: S::Type,
-    pub attachment_map: M::Map<String, AttachmentMetadata>,
+    pub attachment_map: M::Map<String, A::Type>,
 }
 
+pub type OutgoingMessageGenericEntity = OutgoingMessageGeneric<LocalString, LocalMap, LocalAttachmentInfo>;
+
+// ========== Outgoing Message Response ==========
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct OutgoingMessageResponseGeneric<S, M>
 where
     S: StringKind,
@@ -34,20 +163,31 @@ where
     pub attachment_upload_id_map: M::Map<String, u32>,
 }
 
+pub type OutgoingMessageResponseEntity = OutgoingMessageResponseGeneric<LocalString, LocalMap>;
+
+// ========== Attachment Download Request ==========
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AttachmentDownloadRequestGeneric<S>
 where
     S: StringKind,
 {
+    pub channel_id: S::Type,
     pub key: S::Type,
 }
 
+pub type AttachmentDownloadRequestEntity = AttachmentDownloadRequestGeneric<LocalString>;
+
+// ========== Attachment Download Response Header ==========
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct AttachmentDownloadResponseHeader
+pub struct AttachmentDownloadResponseHeaderGeneric<A>
+where
+    A: AttachmentInfoKind,
 {
     pub download_id: u32,
-    pub metadata: AttachmentMetadata,
+    pub metadata: A::Type,
 }
+
+pub type AttachmentDownloadResponseHeaderEntity = AttachmentDownloadResponseHeaderGeneric<LocalAttachmentInfo>;
 
 //========================= Attachment Binary ==========================
 
